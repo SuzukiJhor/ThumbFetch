462c05fe43ac3e01f2a0244f6b2de3d8985459f2
Jhordan S <jhordan.kendy@outlook.com>
2023-11-16 09:40:49 -0300

    Feat: retornando a imagem ao navegador, sugerindo o download com o nome "thumbnail.jpg".

--- download.php
+++ download.php
@@ -2,5 +2,13 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $imurl = $_POST['imurl'];
-    echo "imurl URL: " . $imurl;
+    $ch = curl_init($imurl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $download = curl_exec($ch);
+  
+    curl_close($ch);
+    header('Content-type: image/jpg');
+    header('Content-Disposition: attachment; filename="thumbnail.jpg');
+
+    echo $download;
 }
