149445c51da0271800dd3c1ef1306b456952c67d
Jhordan S <jhordan.kendy@outlook.com>
2023-11-16 09:41:44 -0300

    Fix: ajuste de identação

--- download.php
+++ download.php
@@ -2,11 +2,12 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $imurl = $_POST['imurl'];
+
     $ch = curl_init($imurl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $download = curl_exec($ch);
-  
     curl_close($ch);
+    
     header('Content-type: image/jpg');
     header('Content-Disposition: attachment; filename="thumbnail.jpg');
 
